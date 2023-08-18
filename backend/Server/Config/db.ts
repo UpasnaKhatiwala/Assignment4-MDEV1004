63e522490c7d5fdf3b6963cc32682cc95c2fa977
Upasna Khatiwala <200573736@student.georgianc.on.ca>
2023-08-18 13:06:51 -0400

    correct json data and pushed it to mongodb atlas

--- backend/Server/Config/db.ts
+++ backend/Server/Config/db.ts
@@ -1,5 +1,5 @@
 let localURI= 'mongodb://127.0.0.1:27017/movies';
-let remoteURI='mongodb+srv://upasnakhatiwala3:Himanshu2704@moviecluster.euxdcjj.mongodb.net/movies';
+let remoteURI='mongodb+srv://upasnakhatiwala3:assignment4@movies.upokyeg.mongodb.net/Movies';
 let secret = 'SomeSecret';
 
 export default {
